bc94a68e95949c9d9f251fd3e3d0a60a044000a0
samet <samet@SAMET>
2022-11-10 05:46:58 +0300

    Add project files.

---
+++ Methods/BasketManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Methods
+{
+    class BasketManager
+    {   //naming convetion
+        //sytax
+        public void Add(Product product)
+        {
+            Console.WriteLine("Sepete eklendi" + product.Adi);
+        }
+    }
+}
---
+++ Methods/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   // Dont repeat yourself -DRY
+            // Clean code
+            // Best Practice (Doğru uygulama teknikleri)
+            // Class isimlendirmesi PascalCase yazılır
+
+            Product product1 = new Product();
+            product1.Adi = "Elma";
+            product1.Fiyati = 15;
+            product1.Aciklama = "Amasya elması";
+
+            Product product2 = new Product();
+            product2.Adi = "Karpuz";
+            product2.Fiyati = 80;
+            product2.Aciklama = "Dıyarbakır karpuzu";
+
+            Product[] products = new Product[] { product1, product2 };
+            // type safe -- tip güvenli
+            foreach (Product product in products)
+            {
+                Console.WriteLine(product.Adi);
+                Console.WriteLine(product.Fiyati);
+                Console.WriteLine(product.Aciklama);
+                Console.WriteLine("---------------------------");
+            }
+
+            Console.WriteLine("---------Metodlar-----------");
+
+
+            // instance --örnek
+            // encapsulation
+            BasketManager basketManager = new BasketManager();
+            basketManager.Add(product1);
+            basketManager.Add(product2);
+
+        }
+    }
+}
